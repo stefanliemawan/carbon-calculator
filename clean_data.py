33c279317c093c41bc17c8c877c29b83926f4e38
stefanliemawan <stefanliemawan@gmail.com>
2022-05-22 07:23:17 +0100

    [*] modified clean_data.py to include index

--- clean_data.py
+++ clean_data.py
@@ -7,8 +7,12 @@ footprint_by_item_df = pd.read_excel(
     usecols="B:E"
 )
 
-footprint_by_item_df.rename(columns={"Unnamed: 1" : "Item"}, inplace=True)
+footprint_by_item_df[["Index", "Item"]] = footprint_by_item_df["Unnamed: 1"].str.split(n=1, expand=True)
 
-footprint_by_item_df["Item"] = footprint_by_item_df["Item"].apply(lambda x: " ".join(x.split(" ")[1:]))
+columns = footprint_by_item_df.columns.tolist()
+
+columns = columns[-2:] + columns[1:-2]
+
+footprint_by_item_df = footprint_by_item_df[columns]
 
 footprint_by_item_df.to_csv("./clean_datasets/footprint_by_item.csv", index=False)
