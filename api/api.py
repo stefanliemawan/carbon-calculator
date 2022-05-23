71b6ff2d3daa413f4c3c128a9ad1ad2861de42c4
stefanliemawan <stefanliemawan@gmail.com>
2022-05-23 05:20:08 +0100

    [*] added .env

--- api/api.py
+++ api/api.py
@@ -1,8 +1,18 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+
+PSQL_USERNAME = os.environ.get("PSQL_USERNAME")
+PSQL_PASSWORD = os.environ.get("PSQL_PASSWORD")
+PSQL_HOST = os.environ.get("PSQL_HOST")
+PSQL_PORT = os.environ.get("PSQL_PORT")
+PSQL_API = os.environ.get("PSQL_API")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:10posKamehameha!@localhost:5432/carboncal"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{PSQL_USERNAME}:{PSQL_PASSWORD}@{PSQL_HOST}:{PSQL_PORT}/{PSQL_API}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
